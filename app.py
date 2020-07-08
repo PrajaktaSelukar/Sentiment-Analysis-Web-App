fe65f700f03e4e455592c19f6b70eff4c444899b
PRAJAKTA SELUKAR <34501121+PrajaktaSelukar@users.noreply.github.com>
2020-07-08 16:14:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,21 +24,19 @@ def my_form_post():
     #convert to lowercase
     text1 = request.form['text1'].lower()
     
-    text2 = ''.join(c for c in text1 if not c.isdigit())
+    text_final = ''.join(c for c in text1 if not c.isdigit())
     
     #remove punctuations
-    text3 = ''.join(c for c in text2 if c not in punctuation)
+    #text3 = ''.join(c for c in text2 if c not in punctuation)
         
     #remove stopwords    
-    processed_doc1 = ' '.join([word for word in text3.split() if word not in stop_words])
+    processed_doc1 = ' '.join([word for word in text_final.split() if word not in stop_words])
 
     sa = SentimentIntensityAnalyzer()
     dd = sa.polarity_scores(text=processed_doc1)
     compound = round((1 + dd['compound'])/2, 2)
 
-    return render_template('form.html', final=compound, text1=text3,text2=dd['pos'],text5=dd['neg'],text4=compound,text3=dd['neu'])
+    return render_template('form.html', final=compound, text1=text_final,text2=dd['pos'],text5=dd['neg'],text4=compound,text3=dd['neu'])
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=5002, threaded=True)
-
-    
